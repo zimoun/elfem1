2f071bdc579961d4161068424413a1a35373ec94
zimoun <zimoun.toutoune@gmail.com>
2018-07-03 23:11:20 +0200

    up 07 -- test is still not updated!

---
+++ math.el
@@ -0,0 +1,64 @@
+
+(defun math/abs (number)
+  "Return absolute value of the NUMBER.
+
+If NUMBER is greater than zero, then the function returns NUMBER,
+Else, the function return the converted -NUMBER to `float'"
+  (if (>= number 0)
+      number
+    (- 0.0 number)))
+
+(defvar math/sqrt-tolerance 1e-4
+  "Fix the maximal aboslute gap between two computations of `math/sqrt-recursive-lisp'")
+
+(defun math/sqrt-recursive-lisp (number &optional tolerance current)
+  "Return the square root of the NUMBER.
+
+Pure Lisp implementation, using recursively a fixed-point algorithm to solve: x^2=number,
+i.e., recursively updates by (/ (+ current number) (+ current 1.0))
+until the reached TOLERANCE.
+
+The optionnal parameter TOLERANCE is set by default to `math/sqrt-tolerance'.
+CURRENT is by default set to NUMBER.
+
+WARNING:
+(see `max-lisp-eval-depth' and `max-specpdl-size')
+
+The number of loop-recursion is limited (see elisp specifications).
+Therefore, this function cannot evaluate the square root of number greater
+than approximatively (/ max-specpdl-size 3).
+It is then recommended to use `math/sqrt' instead."
+  (let ((cur current)
+        (tol tolerance)
+        update)
+
+    (when (eq current nil)
+      (setq cur number))
+    (when (eq tolerance nil)
+      (setq tol math/sqrt-tolerance))
+
+    (setq update (/ (+ cur number) (+ cur 1.0)))
+
+    (if (< (math/abs (- update cur)) tol)
+        update
+      (math/sqrt-recursive-lisp number tol update))
+    ))
+
+(defun math/sqrt (number &optional built-in)
+  "Return the square root of the NUMBER
+
+If NUMBER is greater than (/ max-specpdl-size 3),
+then it applies the built-in C function `sqrt',
+else it uses the pure lisp implementation `math/sqrt-recursive-lisp'.
+
+If BUILT-IN is not `nil', then `sqrt' is applied for any NUMBER."
+  (let ((val 0))
+    (when (eq built-in nil)
+        (setq val (/ max-specpdl-size 3)))
+
+    (if (> number val)
+        (sqrt number)
+      (math/sqrt-recursive-lisp number))
+    ))
+
+(provide 'math)
